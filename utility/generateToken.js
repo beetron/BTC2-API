a0ea0366762d7c378c49d5145d7e65820f055892
Brian <bdev@btro.jp>
2025-01-07 19:39:15 +0900

    Removed res.cookie due to frontend migrating from ReactJS to React Native mobile.

--- utility/generateToken.js
+++ utility/generateToken.js
@@ -1,21 +1,11 @@
 import jwt from "jsonwebtoken";
 
-const generateTokenAndReturn = (userId, res) => {
+const generateToken = (userId) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: "7d",
   });
 
-  return res.status(200).json({ token });
-  // return res
-  //   .status(200)
-  //   .json({ message: "User logged in successfully", token });
-
-  // res.cookie("jwt", token, {
-  //   maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days to milliseconds
-  //   httpOnly: true, // Counter Cross-Site scripting (XSS) attacks
-  //   sameSite: "strict", // Counter Cross-Site Request Forgery (CSRF)
-  //   secure: process.env.NODE_ENV !== "development",
-  // });
+  return token;
 };
 
-export default generateTokenAndReturn;
+export default generateToken;
