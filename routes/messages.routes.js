1903df14e9fe81adcba35c3d4032e5737c13e62f
Brian <bdev@btro.jp>
2025-05-03 18:20:05 +0900

    deleteMessages added to message controller

--- routes/messages.routes.js
+++ routes/messages.routes.js
@@ -1,11 +1,15 @@
 import express from "express";
-import { sendMessage } from "../controllers/message.controller.js";
-import { getMessages } from "../controllers/message.controller.js";
+import {
+  sendMessage,
+  getMessages,
+  deleteMessages,
+} from "../controllers/message.controller.js";
 import protectRoute from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
 router.post("/send/:id", protectRoute, sendMessage);
 router.get("/get/:id", protectRoute, getMessages);
+router.put("/delete/:id", protectRoute, deleteMessages);
 
 export default router;
