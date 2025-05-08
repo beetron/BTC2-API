a5b531cf6731e1bc6ae28c0b0e3ea5e8d383513b
Brian <bdev@btro.jp>
2025-05-08 19:33:28 +0900

    Update generateToken.js

--- utility/generateToken.js
+++ utility/generateToken.js
@@ -3,6 +3,7 @@ import jwt from "jsonwebtoken";
 const generateToken = (userId) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: "7d",
+    // expiresIn: "30m",
   });
 
   return token;
