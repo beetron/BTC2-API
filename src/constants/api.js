c551c8c9629969b0a3a77244652969bf58739c5b
Brian <bdev@btro.jp>
2025-05-14 21:16:10 +0900

    Removed unused code/file

--- src/constants/api.js
+++
@@ -1,8 +0,0 @@
-import dotenv from "dotenv";
-dotenv.config();
-
-// Get API URL
-export const API_URL =
-  process.env.NODE_ENV === "development"
-    ? process.env.DEVELOPMENT_URL
-    : process.env.PRODUCTION_URL;
